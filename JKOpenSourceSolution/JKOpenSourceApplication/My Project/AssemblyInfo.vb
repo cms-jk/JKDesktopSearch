51680db66b963391ff7b8e0d1c2d520c47c08f76
cms-jk <cms-jk@cmsc.co.jp>
2012-04-03 10:12:44 +0900

    add My Project

---
+++ JKOpenSourceSolution/JKOpenSourceApplication/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。 
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します。
+
+<Assembly: AssemblyTitle("JKDeskTopSearch")> 
+<Assembly: AssemblyDescription("JapanKnowledgeのデスクトップ検索用アプリです")> 
+<Assembly: AssemblyCompany("佛教大学図書館")> 
+<Assembly: AssemblyProduct("JK DeskTop Search")> 
+<Assembly: AssemblyCopyright("Powered by 佛大図書館 K. IINO Y. INOUE Assit by CMS Use CustomTabControl by The Man from U.N.C.L.E.")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("bdeb9523-6477-4033-a01f-b2dece1b2c0f")> 
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.5.1.0")> 
+<Assembly: AssemblyFileVersion("1.5.1.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("ja-JP")> 
